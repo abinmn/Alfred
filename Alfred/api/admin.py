41c3062db6a08826800b84c0fbd411e733aa59ff
Abin Mn <abinmn619@gmail.com>
2019-09-14 11:11:23 +0530

    Modified some relations in Models

--- Alfred/api/admin.py
+++ Alfred/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+admin.site.register(ExcelID)
+admin.site.register(College)
+admin.site.register(Event)
+admin.site.register(Event_Participants)
+admin.site.register(Team)
--- Alfred/api/models.py
+++ Alfred/api/models.py
@@ -12,46 +12,50 @@ class ExcelID(models.Model):
     id = models.CharField(primary_key = True, max_length=10)
     #references college table
     name = models.CharField("Student Name",max_length = 100)
-    college = models.OneToOneField(College, on_delete=models.CASCADE)
-    email = models.EmailField("Email", max_length=254)
-    phone_number = models.IntegerField("Phone Number")
+    college = models.ForeignKey(College, on_delete=models.CASCADE, related_name="students")
+    email = models.EmailField("Email", max_length=254, unique=True)
+    phone_number = models.IntegerField("Phone Number", unique=True)
     is_mecian = models.BooleanField(default=False)
-    price = models.IntegerField()
+    is_FullAccess = models.BooleanField(default=True)
+    price = models.IntegerField(default=0)
 
     def __str__(self):
-        return "%s - %s" % (self.excel_id, self.excel_id.name)
+        return "%s - %s" % (self.id, self.name)
 
 class Event(models.Model):
     id = models.CharField(primary_key = True, max_length=10)
     name = models.CharField("Event Name", max_length=50)
-    rules = models.TextField("Event Rules")
-    venue = models.CharField(max_length = 100)
+    rules = models.TextField("Event Rules", blank=True)
+    venue = models.CharField(max_length = 100, blank=True)
     is_paid = models.BooleanField("Paid Event", default=False)
     is_team = models.BooleanField("Team Event", default=False)
-    start_time = models.DateTimeField("Event Start Time", auto_now=False, auto_now_add=False)
-    end_time = models.DateTimeField("Event End Time", auto_now=False, auto_now_add=False)
-    base_price = models.IntegerField()
-    mec_price = models.IntegerField()
+    start_time = models.DateTimeField("Event Start Time", null=True, blank=True)
+    end_time = models.DateTimeField("Event End Time", null=True, blank=True)
+    base_price = models.IntegerField(default=0)
+    mec_price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
 
 class Event_Participants(models.Model):
-    event = models.ManyToManyField(Event, verbose_name=("Event Id"), related_name="event")
-    excel_id = models.ManyToManyField(ExcelID, related_name="excelID")
+    event = models.ForeignKey(Event, verbose_name=("Event Id"), on_delete=models.CASCADE)
+    excel_id = models.ForeignKey(ExcelID, related_name="excelID", on_delete=models.CASCADE)
     #Applicable only for paid events
     have_paid = models.BooleanField()
     is_shortListed = models.BooleanField()
     is_winner = models.BooleanField()
-    winner_position = models.IntegerField()
+    winner_position = models.IntegerField(blank=True)
+
+    class Meta:
+        verbose_name = "Event Participant"
 
     def __str__(self):
         return "%s - %s" % (self.excel_id, self.excel_id.name)
 
 class Team(models.Model):
-    team_id = models.CharField(max_length=32)
+    team_id = models.CharField(max_length=32)   
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     excel_id = models.ManyToManyField(ExcelID, related_name="student")
 
     def __str__(self):
-        return "%s %s %s" % (self.team_id, self.event.name, self.excel_id.name)
+        return "%s %s" % (self.team_id, self.event.name)
